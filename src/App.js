c040219478387858f71ecf5ecfc22c655875a8af
Owain Stratton <owain@globalmouth.com>
2017-03-30 21:20:51 +0200

    cleaned up master as starting point for the workshop

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from 'react';
 import 'normalize.css';
 import './App.css';
 
-import ChartWidget from './ChartWidget';
-
 const App = () => (
    <div className="d3-app">
       <div className="d3-app--header">
          <h1>D3.js and React</h1>
       </div>
-      <ChartWidget />
+      
    </div>
 )
 
